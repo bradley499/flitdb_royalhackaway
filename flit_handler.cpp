28178bda7580fa1c88fdeac3e8b975b61d18df3e
Bradley Marshall <bradm499@gmail.com>
2021-02-06 12:48:24 +0000

    Moved function from header

--- flit_handler.cpp
+++ flit_handler.cpp
@@ -2,6 +2,16 @@
 #ifndef flit_handler_cpp
 #define flit_handler_cpp
 
+#include "flit_handler.h"
 
+int flitdb::setup(const char *filename, int flags)
+{
+	if (configured)
+	{
+		err_message = "The database handler has already been attributed to handle another database\0";
+		return FLITDB_ERROR;
+	}
+	return FLITDB_SUCCESS;
+}
 
 #endif
--- flit_handler.h
+++ flit_handler.h
@@ -25,14 +25,4 @@ flitdb::~flitdb()
 {
 }
 
-int flitdb::setup(const char *filename, int flags)
-{
-	if (configured)
-	{
-		err_message = "The database handler has already been attributed to handle another database\0";
-		return FLITDB_ERROR;
-	}
-	return FLITDB_SUCCESS;
-}
-
 #endif
